5d06b97f9c1c863c0248f4039e534f5bae1678b1
AshileySabah <ashileysabrina2002@gmail.com>
2026-01-22 17:49:25 -0300

    feat: login

--- src/app/features/auth/pages/login/login.component.ts
+++ src/app/features/auth/pages/login/login.component.ts
@@ -1,9 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,8 +21,39 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+  ) {}
+
+  loading = false;
+
+  form = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit(): void {
+    if (this.form.invalid) return;
+
+    this.loading = true;
+
+    const { username, password } = this.form.getRawValue();
+
+    setTimeout(() => {
+      if (username === 'sup' && password === '123456') {
+        localStorage.setItem('auth', 'true');
+        this.router.navigate(['/fila-conferencia']);
+      } else {
+        alert('Usuário ou senha inválidos');
+        this.loading = false;
+      }
+    }, 1000);
+  }
+}
