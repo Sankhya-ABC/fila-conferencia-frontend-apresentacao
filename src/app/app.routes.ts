37c19bc0ce2f3c3c73cd9888c50451f631332077
AshileySabah <ashileysabrina2002@gmail.com>
2026-01-22 15:27:56 -0300

    feat: styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { FilaConferenciaComponent } from './features/fila/pages/fila-conferencia/fila-conferencia.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    title: 'Login',
+    component: LoginComponent,
+  },
+  {
+    path: 'fila-conferencia',
+    title: 'Fila de Conferência',
+    component: FilaConferenciaComponent,
+  },
+];
