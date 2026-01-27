842beb99d480f6e1621c5bb86e786c900034c9cb
AshileySabah <ashileysabrina2002@gmail.com>
2026-01-27 19:29:51 -0300

    feat: component: modal

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,26 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, TemplateRef } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { MatIcon } from '@angular/material/icon';
 
-export interface FeedbackDialogData {
-  titulo: string;
-  mensagem: string;
-  tipo?: 'erro' | 'aviso' | 'info';
-  botaoConfirmar?: string;
+export interface ModalProps {
+  content: TemplateRef<any>;
+  context?: any;
 }
 
 @Component({
   selector: 'app-modal',
   standalone: true,
-  imports: [CommonModule, MatIcon],
+  imports: [CommonModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss',
 })
 export class ModalComponent {
   constructor(
     private dialogRef: MatDialogRef<ModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: FeedbackDialogData,
+    @Inject(MAT_DIALOG_DATA) public data: ModalProps,
   ) {}
 
   fechar() {
