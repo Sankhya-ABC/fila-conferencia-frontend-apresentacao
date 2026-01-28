e892e513864fe3be4e062edf8442e3225102107d
AshileySabah <ashileysabrina2002@gmail.com>
2026-01-28 17:39:38 -0300

    feat: FilaConferenciaDTO

--- src/app/services/fila-conferencia/fila-conferencia.model.ts
+++ src/app/services/fila-conferencia/fila-conferencia.model.ts
@@ -1,38 +1,14 @@
-export enum Status {
-  AGUARDANDO_CONFERENCIA = 'Aguardando Conferência',
-  EM_ANDAMENTO = 'Em Andamento',
-  AGUARDANDO_RECONTAGEM = 'Aguardando Recontagem',
-  RECONTAGEM_EM_ANDAMENTO = 'Recontagem em Andamento',
-  FINALIZADO = 'Finalizado',
-}
-
-export enum TipoMovimento {
-  COMPRA = 'Compra',
-  PEDIDO_VENDA = 'Pedido de Venda',
-}
-
-export enum TipoOperacao {
-  NOTA_FISCAL_PRODUTO_S_PD = 'Nota Fiscal - Produto (S/PD)',
-  NOTA_FISCAL_PRODUTO = 'Nota Fiscal - Produto',
-  CUBAGEM_PEDIDO = 'Cubagem de Pedido',
-}
-
-export enum TipoEntrega {
-  TRANSPORTADORA = 'Transportadora',
-}
-
 export interface FilaConferenciaDTO {
+  numeroUnico: string;
+  numeroNota: string;
+  numeroModial: string;
+
+  valorNota: string;
+  volume: string;
+  dataMovimento: string;
+
   codigoStatus: string;
   descricaoStatus: string;
-
-  idEmpresa: string;
-  nomeEmpresa: string;
-
-  numeroModial: string;
-  numeroNota: string;
-  numeroUnico: string;
-
-  dataMovimento: Date;
 
   codigoTipoMovimento: string;
   descricaoTipoMovimento: string;
@@ -43,18 +19,20 @@ export interface FilaConferenciaDTO {
   codigoTipoEntrega: string;
   descricaoTipoEntrega: string;
 
+  idEmpresa: string;
+  nomeEmpresa: string;
+
   idParceiro: string;
   nomeParceiro: string;
 
   idVendedor: string;
   nomeVendedor: string;
 
-  valorNota: string;
-
-  volume: string;
-
   idUsuarioInclusao: string;
+  nomeUsuarioInclusao: string;
+
   idUsuarioAlteracao: string;
+  nomeUsuarioAlteracao: string;
 }
 
 export interface ParceiroDTO {
