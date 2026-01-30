2c538399d7461d4175bb9cca947ba45c0505731c
AshileySabah <ashileysabrina2002@gmail.com>
2026-01-30 10:43:57 -0300

    feat: money mask

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+
+registerLocaleData(localePt);
 
 bootstrapApplication(AppComponent, {
   ...appConfig,
@@ -14,5 +18,6 @@ bootstrapApplication(AppComponent, {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue: { subscriptSizing: 'dynamic' },
     },
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
 }).catch(console.error);
